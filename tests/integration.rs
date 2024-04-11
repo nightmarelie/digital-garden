ca4a1499f9861de6837c7e030304c31a92d16c02
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-11 16:20:49 -0600

    test: add a few integrations

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,9 +1,30 @@
 use assert_cmd::Command;
+use color_eyre::eyre::Result;
 
 #[test]
-fn test_help() {
-    let mut cmd = Command::cargo_bin("garden").unwrap();
+fn test_help() -> Result<()> {
+    let mut cmd = Command::cargo_bin("garden")?;
     let assert = cmd.arg("--help").assert();
-
     assert.success().stderr("");
+
+    Ok(())
+}
+
+#[test]
+fn test_write_help() -> Result<()> {
+    let mut cmd = Command::cargo_bin("garden")?;
+    let assert = cmd.arg("write").arg("--help").assert();
+    assert.success().stderr("");
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn test_write() -> Result<()> {
+    let mut cmd = Command::cargo_bin("garden")?;
+    let assert = cmd.arg("write").assert();
+    assert.success().stderr("");
+
+    Ok(())
 }
