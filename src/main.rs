81e88616fd6f5c97ac68d084e145dd73d31fd6cb
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-11 15:52:31 -0600

    doc: add documentation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 use color_eyre::eyre::Result;
 use structopt::StructOpt;
 
+/// A CLI for the growing and curation of a digital garden
 #[derive(Debug, StructOpt)]
 #[structopt(name = "garden", about = "A CLI tool for creating and managing a digital garden.")]
 struct Opt {
@@ -11,7 +12,11 @@ struct Opt {
 
 #[derive(StructOpt, Debug)]
 enum Command {
+    /// write something in yur garden
+    ///
+    /// This command will open your $EDITOR, wait for you to write something, and then save it to your garden.
     Write {
+        /// Optionally specify a title for what you're writing about
         #[structopt(short, long)]
         title: Option<String>
     },
