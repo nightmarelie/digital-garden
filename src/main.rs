2870f472d4129c393b037a5263adcd9fb1bb9936
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-11 15:48:09 -0600

    feat: add base command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::str::FromStr;
 use color_eyre::eyre::Result;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "garden", about = "A CLI tool for creating and managing a digital garden.")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    Write {
+        #[structopt(short, long)]
+        title: Option<String>
+    },
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    let opt = Opt::from_args();
+    dbg!(opt);
+
     todo!();
 }
 
