dd8313ca40e023b02cd6c3ebc2839fa89c11fd2a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-13 21:08:25 -0600

    feat: low level build

--- src/write.rs
+++ src/write.rs
@@ -1,10 +1,28 @@
+use std::io::{Read, Write};
 use std::path::PathBuf;
-use color_eyre::eyre::{Result, WrapErr};
 
-pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
-    dbg!(garden_path, title);
-    let template = "# ";
-    let content_from_user = edit::edit(template).wrap_err("unable to read writing")?;
-    dbg!(content_from_user);
+use color_eyre::eyre::{Result, WrapErr};
+use edit::{Builder, edit_file};
+
+const TEMPLATE: &[u8; 2] = b"# ";
+
+
+pub fn write(garden_path: PathBuf, _title: Option<String>) -> Result<()> {
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create wip file")?
+        .keep()
+        .wrap_err("Failed to keep tempfile")?;
+    file.write_all(TEMPLATE)?;
+    // let the user write whatever they want in their favorite editor
+    // before returning to the cli and finishing up
+    edit_file(filepath)?;
+    // Read the user's changes back from the file into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    dbg!(contents);
     todo!()
 }
