2d39ca0c8b3a5b0e7b0a3942d8cc30fd21dde533
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-13 20:56:59 -0600

    feat: add edit dep

--- src/write.rs
+++ src/write.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
-use color_eyre::Result;
+use color_eyre::eyre::{Result, WrapErr};
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
     dbg!(garden_path, title);
+    let template = "# ";
+    let content_from_user = edit::edit(template).wrap_err("unable to read writing")?;
+    dbg!(content_from_user);
     todo!()
 }
